269028f116f018dcc67c13e87f95705d8e8586c4
gdpnei2002 <joseneilson25@gmail.com>
2023-04-11 15:40:18 -0300

    conexão ao bd + teste de inserção

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "banco",
+});
+
+app.get('/', (req, res) =>{
+  db.query(
+    "INSERT INTO usuarios (email, password) VALUES ('jose@gmail.com', '12345678')", (err, result) => {
+      if(err){
+        console.log(err)
+      }
+    }
+    );
+});
+
+app.get('/', (req, res) =>{
+  res.send("Hellooo")
+})
 
 app.listen(3001, () => {
     console.log("rodando na porta 3001");
